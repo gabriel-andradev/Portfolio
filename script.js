027b8940091479c4f06e10c8312d23511e590d3f
Gabriel Andrade <101130704+gabriel-andradev@users.noreply.github.com>
2022-03-31 13:18:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+//function mudaTema() {
+//    document.body.classList.toggle("dark");
+//}
+
+const chk = document.getElementById('chk')
+
+chk.addEventListener('change', () => {
+    document.body.classList.toggle('light')
+})
